02b572473c2fc70294c4095ec6eb84339fe65e91
Vaibhav Bhosale <106335135+MrVaibhavBhosale@users.noreply.github.com>
2025-10-31 13:55:26 +0530

    Register OppvenuzChoiceMaster with ModelAdmin
    Added OppvenuzChoiceAdmin to manage OppvenuzChoiceMaster.

--- admin_master/admin.py
+++ admin_master/admin.py
@@ -1,4 +1,16 @@
-from  django.contrib import admin
-from .models import Service_master
+from django.contrib import admin
+from .models import OppvenuzChoiceMaster, Service_master
 
-admin.site.register(Service_master)
+@admin.register(OppvenuzChoiceMaster)
+class OppvenuzChoiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'choice_name',
+        'minimum_comments_count',
+        'archived_comments_count',
+        'average_percentage',
+        'status',
+        'created_at',
+    )
+    search_fields = ('choice_name',)
+    list_filter = ('status',)
